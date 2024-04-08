6d46f7394ce99f869e7b23b678c122a5e2f3a9e5
Yusif Jabrayilov <mahirliy@Yusifs-MacBook-Pro.local>
2024-04-08 14:08:50 +0200

    init commit

--- components/ui/catalog-card.tsx
+++ components/ui/catalog-card.tsx
@@ -11,7 +11,7 @@ const CatalogCard = ({ catalog }: CatalogCardProps) => {
     <div className="flex flex-col md:w-[262px] w-full justify-center items-center border rounded-lg h-[300px] p-4 gap-y-4 shadow-sm">
       <Image src={catalog.image} alt={catalog.name} width={200} height={200} />
       <span className="font-medium w-[250px] text-center">{catalog.name}</span>
-      <span className="text-start text-xs">40 offers</span>
+      {/* <span className="text-start text-xs">40 offers</span> */}
     </div>
   );
 };
